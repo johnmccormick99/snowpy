d6edd734333c3e058167e7ee7d3593fb12531beb
jmccor99 <31248736+jmccor99@users.noreply.github.com>
2020-03-04 13:38:29 +0000

    Update preDeploy.py

--- preDeploy.py
+++ preDeploy.py
@@ -5,7 +5,7 @@ def main(argv):
 
 	changenumber = argv
 	obj = ServiceNow(changenumber)
-	obj.loopkupChangeRequest(changenumber)
+	obj.loopkupChangeRequest()
 	change_window  = obj.isChangeWindowOpen()
 	approval_state = obj.approvedState()
 
