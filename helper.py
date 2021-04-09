b30b3408fb5ee725d3c5cfc6cff0e71cafa929ea
johnmccormick99 <31248736+johnmccormick99@users.noreply.github.com>
2021-04-09 10:02:20 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -7,7 +7,7 @@ class ServiceNow:
 	def __init__(self, chg=''):
 		self.baseurl = 'https://dev94652.service-now.com/api/now'
 		self.username = 'admin'
-		self.password = 'zHdu9GFIh5Jo'
+		self.password = 'hellojmac'
 		self.payload = ''
 		self.headers = ''
 		self.sysId = ''
